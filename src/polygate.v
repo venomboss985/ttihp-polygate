e7a80cb5a81e228d05286495ef47f49bced55aab
VenomAI <jht8287@gmail.com>
2025-06-05 22:56:54 +0000

    Initial polygate design hardened, needs testing

---
+++ src/polygate.v
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2024 Jake Taylor
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+/*
+    INPUT
+    b0: 0 = or, 1 = and;
+    b1: 0 = Q, 1 = ~Q;
+    b2: A input
+    b3: B input
+
+    OUTPUT
+    b0: reserved for internal buffer;
+    b1: reserved for internal buffer;
+    b2: Q;
+    b3: ~Q;
+*/
+
+module polygate (
+  input  wire       clk_in,
+  input  wire [3:0] in,
+  output reg  [1:0] out
+);
+
+  reg [3:0] in_buf;
+
+  always @(posedge clk_in) begin
+    // Reset input buffer
+    in_buf = 4'b0;
+
+    // OR/AND selection logic
+    if (in[0] == 0)
+      in_buf[0] = in[2] | in[3];
+    else
+      in_buf[0] = in[2] & in[3];
+
+    // INVERT selection logic
+    if (in[1] == 1)
+    begin
+      in_buf[2] = ~in_buf[0];
+      in_buf[3] = in_buf[0];
+    end else
+    begin
+      in_buf[2] = in_buf[0];
+      in_buf[3] = ~in_buf[0];
+    end
+  end
+  
+  assign out[0] = in_buf[2];
+  assign out[1] = in_buf[3];
+endmodule
+
+module tt_um_polygate (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  polygate pg0 (
+    .clk_in (clk),
+    .in (ui_in[3:0]),
+    .out (uo_out[1:0])
+  );
+
+  polygate pg1 (
+    .clk_in (clk),
+    .in (ui_in[7:4]),
+    .out (uo_out[3:2])
+  );
+
+  assign uo_out[7:4] = 0;
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{uio_in, ena, clk, rst_n, 1'b0};
+
+endmodule
